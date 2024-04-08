5476089ad369f8a32d22ccdbcec0dcc513a83666
zflorezz <zakinahf030904@gmail.com>
2024-04-07 20:14:39 -0500

    card details finished

--- src/composables/useAPI.js
+++ src/composables/useAPI.js
@@ -7,6 +7,7 @@ const instance = axios.create({
 
 const employees = ref([])
 const loading = ref(false)
+const currentEmployee = ref(null)
 
 export default function useAPI() {
 
@@ -20,5 +21,10 @@ export default function useAPI() {
         loading.value = false
     }
 
-    return {instance, employees, getEmployees, loading}
+    const fetchEmployee = async (id) => {
+        const response = await instance.get(`api/employees/fetch/${id}`)
+        currentEmployee.value = response.data
+    }
+
+    return {instance, employees, getEmployees, loading, fetchEmployee, currentEmployee}
 }    
--- src/views/CardDetails.vue
+++ src/views/CardDetails.vue
@@ -1,8 +1,33 @@
 <script setup>
+import {ref, onMounted, onUnmounted} from 'vue'
 import { useRoute } from 'vue-router'
+import useAPI from '@/composables/useAPI';
+import { faker } from '@faker-js/faker'
+
+const {fetchEmployee, currentEmployee} = useAPI()
+
+onMounted(async () => {
+    await fetchEmployee(route.params.id)
+})
+
+onUnmounted(() => {
+    currentEmployee.value = null
+})
+
 const route = useRoute()
 </script>
 
 <template>
-    {{route.params.id}}
+    <!-- {{route.params.id}} -->
+    <main>
+        <div v-if="currentEmployee" class="flex flex-col items-center justify-center gap-6">
+            <h1 class="text-6xl font-bold p-5">{{ currentEmployee.firstName }} {{ currentEmployee.lastName }}</h1>
+            <h1 class="text-3xl p-5">{{ currentEmployee.title }}</h1>
+            <h1 class="text-2xl p-5"> {{  currentEmployee.userName }} @southtexascollege.edu</h1>
+            <h1 class="text-2xl p-5"> {{  currentEmployee.quote }}</h1>
+        
+            <img class="p-8" :src="faker.image.urlLoremFlickr({category: 'cat'})" />
+        </div>
+    </main>
+
 </template>
