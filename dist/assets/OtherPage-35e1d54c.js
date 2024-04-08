5efbbc8a3a9aa481dc506e0f5766719107c042a9
zflorezz <zakinahf030904@gmail.com>
2024-04-07 20:26:15 -0500

    adding dist

---
+++ dist/assets/OtherPage-35e1d54c.js
@@ -0,0 +1,1 @@
+import{o,c as r,a}from"./index-3defda3e.js";const _=(e,t)=>{const s=e.__vccOpts||e;for(const[c,n]of t)s[c]=n;return s},i={},f={class:"flex min-h-screen items-center justify-center"},l=a("h1",{class:"text-6xl font-thin text-slate-800"},"I'm Other Page",-1),h=[l];function m(e,t){return o(),r("main",f,h)}const x=_(i,[["render",m]]);export{x as default};
